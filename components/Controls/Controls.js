41dbbacf7b7b18498315b27a2f817e471e93deef
Itai Ariel <itai.a@elasticmedia.io>
2016-09-07 15:48:37 +0300

    gestures with play and pause

--- components/Controls/Controls.js
+++ components/Controls/Controls.js
@@ -3,32 +3,49 @@ import Hammer from '../../utils/hammer.min';
 import ReactDOM from 'react-dom';
 
 var Controls = React.createClass({
+  componentWillMount(){
+    this.state = {isPlaying: true};
+  },
   componentDidMount(){
     this.gestureListener = new Hammer(ReactDOM.findDOMNode(this));
-    console.log("setting listener!!!!!!!!!!!!!!!");
-    this.gestureListener.on("swipeleft swiperight", function(ev) {
-      console.log(ev.type +" gesture detected.");
-    });
+    this.gestureListener.on("swipeleft swiperight", this.swipeEvent.bind(this));
+  },
+  swipeEvent(event) {
+    if (event.type === "swipeleft") {
+      this.props.stateMachine.eventHandler("next");
+    } else if (event.type === "swiperight") {
+      this.props.stateMachine.eventHandler("previous");
+    }
+  },
+  getClassName(name){
+    var className = 'controller';
+    if (name === "play") {
+      className += this.state.isPlaying ? ' hidden' : '';
+    } else {
+      className += !this.state.isPlaying ? ' hidden' : '';
+    }
+    return className;
+
   },
   eventHandler(event){
+    if (event.target.id === "play" || event.target.id === "pause") {
+      this.setState({isPlaying: !this.state.isPlaying});
+    }
     this.props.stateMachine.eventHandler(event.target.id);
   },
   render(){
     return (
       <div className="controls playerHolder">
-        <img src="images/logo.png" className="controler" id="extend" onClick={this.eventHandler}/>
-          <span>
-            <button id="play" onClick={this.eventHandler}>Play</button>
-          </span>
-          <span>
-            <button id="pause" onClick={this.eventHandler}>Pause</button>
-          </span>
-          <span>
-            <button id="next" onClick={this.eventHandler}>next</button>
-          </span>
-          <span>
-            <button id="previous" onClick={this.eventHandler}>previous</button>
-          </span>
+        <img src="images/logo.png" className="controller" id="extend" onClick={this.eventHandler}/>
+        <img src="images/play.png" className={this.getClassName("play")} id="play" onClick={this.eventHandler}/>
+        <img src="images/pause.png" className={this.getClassName("pause")} id="pause" onClick={this.eventHandler}/>
+        <span id="dots">
+          <img src="images/dot.png" className="dot" id="dot1"/>
+          <img src="images/dot.png" className="dot" id="dot2"/>
+          <img src="images/dot.png" className="dot" id="dot3"/>
+          <img src="images/dot.png" className="dot" id="dot4"/>
+          <img src="images/dot.png" className="dot" id="dot5"/>
+        </span>
       </div>
     );
   }
