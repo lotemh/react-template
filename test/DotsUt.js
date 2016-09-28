1b2ac95db8b5199ba9911ccf62897938d3a431c9
lotemh <lotem.h@elasticmedia.io>
2016-09-28 12:07:56 +0300

    fix Dots props

--- test/DotsUt.js
+++ test/DotsUt.js
@@ -8,7 +8,7 @@ describe('Dots test suite', () => {
 
     it('test max number of dots in a page', () => {
         const component = TestUtils.renderIntoDocument(
-            <Dots inExtend="false" itemNum="1" numOfItems="5" />
+            <Dots inExtend={false} itemNum={1} numOfItems={5} />
         );
         const domNode = ReactDOM.findDOMNode(component);
         expect(domNode.children.length).to.be.equal(5);
