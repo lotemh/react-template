1071bfaa673cfdeb58f23ea34f41c7097f1f08f9
lotemh <lotem.h@elasticmedia.io>
2016-10-27 17:10:55 +0300

    allow working with different clients

--- components/demoClients/kcetDemo.js
+++ components/demoClients/kcetDemo.js
@@ -2,22 +2,23 @@
  * Created by user on 9/7/2016.
  */
 import React, { PropTypes } from 'react';
-import VideoElement from '../VideoElement/VideoElement';
 import BrightCovePlayer from '../VideoElement/brightCovePlayer';
-import Controls from '../Controls/Controls';
 import ElasticMediaSdk from '../ElasticMediaSdk/ElasticMediaSdk';
 
 const numOfPlayers = 2;
 
 const Kcet = React.createClass({
+    propTypes: {
+        contentUrl: PropTypes.string
+    },
     render() {
         const players = new Array(numOfPlayers).fill(0);
         return (
-      <ElasticMediaSdk>
+      <ElasticMediaSdk contentUrl={this.props.contentUrl}>
           {
             players.map((elm, i) => {
                 return (
-                <BrightCovePlayer key={`player${i}`} playerId={`player${i}`} />
+                <BrightCovePlayer key={`player${i}`} playerId={`player${i}`} {...this.props}/>
               );
             })
           }
--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 /**
- * React Static Boilerplate
- * https://github.com/kriasoft/react-static-boilerplate
- *
- * Copyright © 2015-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
+ * Created by user on 10/9/2016.
  */
 
 import 'babel-polyfill';
@@ -15,30 +9,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import FastClick from 'fastclick';
 import { Provider } from 'react-redux';
+import Kcet from './components/demoClients/kcetDemo';
 
 import store from './core/store';
-import router from './core/router';
-import history from './core/history';
 
-let routes = require('./routes.json'); // Loaded with utils/routes-loader.js
-const container = document.getElementById('container');
+const container = document.querySelector('[data-elastic-media-account]');
 
-function renderComponent(component) {
-  ReactDOM.render(<Provider store={store}>{component}</Provider>, container);
+var clientMap = {
+    '1234': Kcet
+};
+
+function getClientByAccount(account) {
+    return clientMap[account];
 }
 
-// Find and render a web page matching the current URL path,
-// if such page is not found then render an error page (see routes.json, core/router.js)
-function render(location) {
-  router.resolve(routes, location)
-    .then(renderComponent)
-    .catch(error => router.resolve(routes, { ...location, error }).then(renderComponent));
+function getClient(container) {
+    var account = container.getAttribute('data-elastic-media-account');
+    var props = {};
+    var playerAttributesList = [];
+    for (var i=0; i < container.attributes.length; i++){
+        var attr = container.attributes[i];
+        if (attr.nodeName === 'data-video-id'){
+            props.contentUrl = attr.nodeValue;
+        }
+        props[attr.nodeName] = attr.nodeValue;
+    }
+    var client = getClientByAccount(account);
+    return React.createElement(client, props);
 }
 
-// Handle client-side navigation by using HTML5 History API
-// For more information visit https://github.com/ReactJSTraining/history/tree/master/docs#readme
-history.listen(render);
-render(history.getCurrentLocation());
+var client = getClient(container);
+
+function render(client) {
+    ReactDOM.render(<Provider store={store}>{client}</Provider>, container);
+}
+
+render(client);
 
 // Eliminates the 300ms delay between a physical tap
 // and the firing of a click event on mobile browsers
@@ -47,8 +53,6 @@ FastClick.attach(document.body);
 
 // Enable Hot Module Replacement (HMR)
 if (module.hot) {
-  module.hot.accept('./routes.json', () => {
-    routes = require('./routes.json'); // eslint-disable-line global-require
-    render(history.getCurrentLocation());
-  });
+    render();
 }
+
