0bb56ff71b25ea6c76e06b1f249fc7b2d9d28750
lotemh <lotem.h@elasticmedia.io>
2016-11-03 11:39:23 +0200

    remove commented out code

--- components/ElasticMediaSdk/ElasticMediaSdk.js
+++ components/ElasticMediaSdk/ElasticMediaSdk.js
@@ -15,7 +15,6 @@ const ElasticMediaSdk = React.createClass({
         if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
             pendingFirstPlayClick = true;
         }
-        // this.state = this.calcWidthAndHeight();
         this.stateMachine = new StateMachine(pendingFirstPlayClick);
     },
 
@@ -52,33 +51,6 @@ const ElasticMediaSdk = React.createClass({
 
         ////// window.addEventListener('resize', this.handleResize);
     },
-    // calcWidthAndHeight() {
-    //     let result = {
-    //             width: window.innerWidth,
-    //             height: window.innerHeight,
-    //             left: 0,
-    //             top: 0
-    //         },
-    //         bestWidth,
-    //         bestHeight,
-    //         ratio = 1.77777778;// TODO get this value from source
-    //     if (result.width < result.height || result.height * ratio > result.width) {
-    //         bestHeight = result.width * (1 / ratio);
-    //         result.top = (result.height - bestHeight) / 2;
-    //         result.height = bestHeight;
-    //     } else {
-    //         bestWidth = result.height * ratio;
-    //         result.left = (result.width - bestWidth) / 2;
-    //         result.width = bestWidth;
-    //     }
-    //     return result;
-    // },
-    // componentWillUnmount() {
-    //     window.removeEventListener('resize', this.handleResize);
-    // },
-    // handleResize() {
-    //     this.setState(this.calcWidthAndHeight());
-    // },
     eventHandler(event){
         this.stateMachine.eventHandler(event);
     },
